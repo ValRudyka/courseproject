d8b893eec8b3d77b4df1c06827be21308971ff32
Valerii Rudyka <val.rudika@gmail.com>
2024-03-16 21:58:13 +0200

    Used env variables

--- mvc/controller.py
+++ mvc/controller.py
@@ -1,9 +1,13 @@
 from PySide6.QtCore import QObject, QCoreApplication
 from PySide6.QtGui import QMovie
+from PySide6.QtWidgets import QMessageBox, QFileDialog
+
 from mvc.model import Model
 from mvc.view.ui_config import MAIN_MENU_DARK, MAIN_MENU_LIGHT
 from mvc.view.mainmenu.mainview import MainView 
 from mvc.view.interface.chartsview import ChartsView
+
+import os
 
 class Controller(QObject):
     def __init__(self, model: Model, main_menu: MainView, charts_view: ChartsView) -> None:
@@ -13,9 +17,37 @@ class Controller(QObject):
         self.charts_view = charts_view
 
         self.main_menu.ui.QPushButton.clicked.connect(self.toggleStyleSheet)
-        self.main_menu.ui.fetchButton.clicked.connect(self.show_charts)
+        self.main_menu.ui.fetchButton.clicked.connect(self.fetch_data)
+        self.main_menu.ui.predictPrices.clicked.connect(self.make_prediction)
+        self.main_menu.ui.clearModel.clicked.connect(self.clear_model)
+        self.main_menu.ui.loadDataset.clicked.connect(self.save_dataset)
 
-    def show_charts(self) -> None:
+        self.charts_view.ui.line_chart_btn.clicked.connect(self.line_chart)
+        self.charts_view.ui.area_chart_btn.clicked.connect(self.area_chart)
+        # self.charts_view.ui.probability_distr_btn.clicked.connect(self.probability_distribution)
+        self.charts_view.ui.scatter_plot_btn.clicked.connect(self.scatter_plot)
+
+    # charts
+    def line_chart(self) -> None:
+        self.charts_view.display_line_chart(self.model.get_actual_data())        
+
+    def area_chart(self) -> None:
+        self.charts_view.display_area_chart(self.model.get_actual_data())
+
+    def probability_distribution(self) -> None:
+        self.charts_view.display_distribution(self.model.get_actual_data())
+
+    def scatter_plot(self) -> None:
+        self.charts_view.display_scatter_plot(self.model.get_actual_data())
+
+    # model and data    
+    def clear_model(self) -> None:
+        self.model.clear_cache(os.getenv('MODEL_CACHE_PATH'))
+
+    def clear_data(self) -> None:
+        self.model.clear_cache(os.getenv('BTC_CACHE_PATH'))
+
+    def fetch_data(self) -> None:
         movie = QMovie('icons\loading_spinner.svg')
         
         self.main_menu.ui.label_15.setMovie(movie)
@@ -26,8 +58,18 @@ class Controller(QObject):
         movie.stop()
         self.main_menu.ui.label_15.setText("Success!")
 
-        self.charts_view.show()
-        self.charts_view.charts_shadow_effects()
+    def save_dataset(self):
+        options = QFileDialog.Options()
+        filename, _ = QFileDialog.getSaveFileName(self.main_menu.ui.MainWindow, "Save Dataset", "", 
+                                                "CSV files (.csv);;Excel files (.xlsx);;All files (*)", options=options)        
+        
+        if filename:
+            try:
+                ending = filename[filename.find('.'):]
+                self.model.save_cache(filename, ending)
+                QMessageBox.information(self.main_menu.ui.MainWindow, 'File has been saved successfully')
+            except Exception as error:
+                QMessageBox.warning(self.main_menu.ui.MainWindow, "Invalid file type", f"An error occured: {str(error)}")
 
     def toggleStyleSheet(self) -> None:
         if self.model.theme == "light":
@@ -44,5 +86,27 @@ class Controller(QObject):
         self.main_menu.setup_shadow_effects()
         self.charts_view.charts_shadow_effects()
 
-    
-        
+    def make_prediction(self) -> None:
+        interval = self.main_menu.ui.spinBox.value()
+        name_interval = self.main_menu.ui.timeIntervals.currentText()
+
+        match name_interval:
+            case 'weeks':
+                interval *= 7 
+            case 'months':
+                interval *= 31
+        try:
+            self.model.train_lstm_model()
+            self.model.predict_model(interval)
+
+            self.charts_view.show()
+            self.charts_view.charts_shadow_effects()
+
+            self.charts_view.display_line_chart(self.model.get_actual_data())
+        except ValueError as val_e:
+            msg = QMessageBox()
+            msg.setText(str(val_e))
+            msg.setWindowTitle("File is empty error")
+            msg.setStandardButtons(QMessageBox.Ok | QMessageBox.Cancel) 
+
+            retval = msg.exec_()
--- mvc/model.py
+++ mvc/model.py
@@ -1,15 +1,15 @@
 from alpha_vantage.cryptocurrencies import CryptoCurrencies
+
 import pandas as pd
 import numpy as np
+import datetime
+import os
+import tensorflow as tf
 
 from sklearn.preprocessing import MinMaxScaler
 
 from keras.models import Sequential, load_model
 from keras.layers import Dense, LSTM
-import tensorflow as tf
-
-import datetime
-import os
 
 np.random.seed(42)
 tf.random.set_seed(42)
@@ -17,26 +17,18 @@ tf.random.set_seed(42)
 class Model:
     def __init__(self) -> None:
         self.__theme = "dark"
-        self.__cache_filename = './cache/btc_daily.csv'
         self.__btc_daily = pd.DataFrame()
         self.__predictions = []   
 
     #private methods:
-    def __load_cache(self) -> None | pd.DataFrame:
+    def __load_cache(self, path: str) -> None | pd.DataFrame:
         try:
-                return pd.read_csv(self.__cache_filename, index_col=0, parse_dates=True)
+                return pd.read_csv(path, index_col=0, parse_dates=True)
         except (IOError, FileNotFoundError):
             return None
 
-    def __save_cache(self, data: pd.DataFrame) -> None:
-        data.to_csv(self.__cache_filename)
-
-    def clear_cache(self, path: str) -> None:
-        with open(path, 'w'):
-            pass
-
     def _is_valid_cache(self) -> bool:
-        data = self.__load_cache()
+        data = self.__load_cache(os.getenv('BTC_CACHE_PATH'))
 
         if (not isinstance(data, pd.DataFrame) or data.empty):
             return False  
@@ -58,7 +50,7 @@ class Model:
         for column in columns:
             self.__btc_daily[column] = pd.to_numeric(self.__btc_daily[column])
 
-        self.__save_cache(self.__btc_daily)
+        self.save_cache(os.getenv('BTC_CACHE_PATH'))
 
     def __pre_train(self, window_size: int) -> np.array:
         series = self.__btc_daily['4b. close (USD)']
@@ -83,10 +75,20 @@ class Model:
     @theme.setter
     def theme(self, value: str) -> None:
         self.__theme = value
+  
+    def save_cache(self, path: str, extension: str = 'csv') -> None:
+        if extension == 'csv':
+            self.__btc_daily.to_csv(path)
+        else:
+            self.__btc_daily.to_excel(path)
+
+    def clear_cache(self, path: str) -> None:
+        with open(path, 'w'):
+            pass
 
     def get_crypto_data(self) -> None:
         if (self._is_valid_cache()):
-            self.__btc_daily = self.__load_cache() 
+            self.__btc_daily = self.__load_cache(os.getenv('BTC_CACHE_PATH')) 
             return None
 
         btc_curr = CryptoCurrencies(key=os.getenv("API_KEY"), output_format='pandas')
@@ -100,7 +102,7 @@ class Model:
             if (self.__btc_daily.empty):
                 raise ValueError('An empty dataset. Have you loaded it firstly before start working?')
 
-            if (os.path.exists('./cache/lstm_model.h5')):
+            if (os.path.exists(os.getenv('MODEL_CACHE_PATH'))):
                 return None
 
             x_train, y_train, _ = self.__pre_train(window_size)
@@ -116,9 +118,9 @@ class Model:
             model.compile(optimizer='adam', loss='mean_squared_error')
             model.fit(x_train, y_train, epochs=10, batch_size=64)
 
-            model.save('./cache/lstm_model.h5')
+            model.save(os.getenv('MODEL_CACHE_PATH'))
         except ValueError as val_e:
-            print(str(val_e))
+            raise ValueError(val_e)
 
     def predict_model(self, num_days: int, window_size: int = 10) -> None:
         _, _, scaler = self.__pre_train(window_size)
@@ -132,7 +134,7 @@ class Model:
 
         X_pred = np.array(X_pred)
 
-        model = load_model('./cache/lstm_model.h5')
+        model = load_model(os.getenv('MODEL_CACHE_PATH'))
 
         predictions = []
         last_window = X_pred[-1] 
@@ -153,7 +155,4 @@ class Model:
         timestamps = self.__btc_daily.index
         x_pred = pd.date_range(start=timestamps[-1], periods=len(self.__predictions)+1, freq='D')[1:]
 
-
         return actual_prices, self.__predictions, x_pred, timestamps
-
-
