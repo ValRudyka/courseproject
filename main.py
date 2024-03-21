f0acd2249ea756fc7ae7f75db7fcd1bc0652b82f
Valerii Rudyka <val.rudika@gmail.com>
2024-03-21 23:47:25 +0200

    Custom changes

--- main.py
+++ main.py
@@ -3,12 +3,11 @@ from dotenv import load_dotenv
 
 from mvc.model import Model
 from mvc.controller import Controller 
-from mvc.view.mainmenu.mainview import MainView
-from mvc.view.interface.chartsview import ChartsView
+from mvc.view.view import View
 
 if __name__ == "__main__":
     load_dotenv()
     
     app = QApplication([])
-    controller = Controller(Model(), MainView(), ChartsView())
+    controller = Controller(Model(), View())
     app.exec()  
